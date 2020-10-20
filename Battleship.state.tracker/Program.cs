cbb3264bd2b220534b44aa705a958b846891aff4
Lencho.Burka <lencho.burka@tigerspike.com>
2020-10-20 19:25:46 +1100

    initial commit

---
+++ Battleship.state.tracker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Battleship.state.tracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
